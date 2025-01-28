94bee3dde87634a64f0ee4bbd64c5afc8bc27db1
pisel <schultz.thomas@free.fr>
2025-01-28 23:47:19 +0100

    wip v2

--- src/main/java/schultz/thomas/discord/bot/business/service/parser/GameServerParser.java
+++ src/main/java/schultz/thomas/discord/bot/business/service/parser/GameServerParser.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface  GameServerParser {
 
     @Mapping(target = "id", ignore = true)
