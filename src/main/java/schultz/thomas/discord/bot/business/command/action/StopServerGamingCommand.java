bccf13074ff9a6984429b9126dd6bf554fea9a9d
pisel <schultz.thomas@free.fr>
2025-02-25 23:16:11 +0100

    tested and working

--- src/main/java/schultz/thomas/discord/bot/business/command/action/StopServerGamingCommand.java
+++ src/main/java/schultz/thomas/discord/bot/business/command/action/StopServerGamingCommand.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import schultz.thomas.discord.bot.business.command.Command;
 import schultz.thomas.discord.bot.business.command.CommandContext;
@@ -18,6 +19,8 @@ import schultz.thomas.discord.bot.model.enums.UserPrivilegeEnum;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Component
 @RequiredArgsConstructor
@@ -27,6 +30,9 @@ public class StopServerGamingCommand implements Command {
 
     private final DockerService dockerService;
 
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public List<UserPrivilegeEnum> roleNeeded(){ return new ArrayList<>( List.of(UserPrivilegeEnum.ADMINISTRATOR, UserPrivilegeEnum.OWNER)); }
 
@@ -49,8 +55,13 @@ public class StopServerGamingCommand implements Command {
         }
         boolean startSuccess = dockerService.stopServer(gamingServerEntity);
         if(!startSuccess){
-            throw new CommandFailedException("Impossible de lancer le serveur de jeu");
+            throw new CommandFailedException("Impossible de pauser le serveur de jeu");
         }
+
+        scheduler.schedule(() -> {
+            applicationEventPublisher.publishEvent(new schultz.thomas.discord.bot.controllers.events.models.GamingServerEvent(this, gamingServerEntity, schultz.thomas.discord.bot.controllers.events.models.GamingServerEvent.GamingServerEventType.SERVER_STATUS_CHANGED));
+        }, 10, java.util.concurrent.TimeUnit.SECONDS);
+
         return "Serveur de jeu est en pause";
     }
 }
