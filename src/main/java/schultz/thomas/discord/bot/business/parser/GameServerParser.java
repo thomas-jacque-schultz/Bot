473428b59c1b18531d1837e9eeaf47b81e5229a2
pisel <schultz.thomas@free.fr>
2025-02-25 22:39:17 +0100

    erreur de mapper

--- src/main/java/schultz/thomas/discord/bot/business/parser/GameServerParser.java
+++ src/main/java/schultz/thomas/discord/bot/business/parser/GameServerParser.java
@@ -15,7 +15,6 @@ public interface  GameServerParser {
 
     //ignored fields are not mapped
     @Mapping(target = "status", ignore = true)
-    @Mapping(target = "running", ignore = true)
     //discord command option with uppercase
     @Mapping(target = "playersMax", source = "players-max")
     @Mapping(target = "urlConnection", source = "url-connection")
